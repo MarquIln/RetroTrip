d03b1f060f3ae884ce1085b7da2e1a712153360d
Marcos Raach <m.raach@edu.pucrs.br>
2025-09-10 14:13:53 -0300

    feat: tests to ci/cd

---
+++ RetroTripTests/RetroTripTests.swift
@@ -0,0 +1,16 @@
+//
+//  RetroTripTests.swift
+//  RetroTripTests
+//
+//  Created by Marcos on 10/09/25.
+//
+
+import Testing
+
+struct RetroTripTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
