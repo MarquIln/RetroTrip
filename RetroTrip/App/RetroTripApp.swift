e34aed42b5aadbd7aefa8569a9a2053bac23bf35
Marcos Raach <m.raach@edu.pucrs.br>
2025-09-12 16:15:38 -0300

    home

--- RetroTrip/App/RetroTripApp.swift
+++ RetroTrip/App/RetroTripApp.swift
@@ -12,7 +12,7 @@ import SwiftData
 struct RetroTripApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView(gameCenter: GameCenterViewModel())
+            ContentView()
         }
     }
 }
